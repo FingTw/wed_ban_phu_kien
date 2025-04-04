af2bcdf5a77374d460a4d8cd71135798ff7072a6
Fingtw <nguyenphwngtinh@gmail.com>
2025-04-04 21:54:00 +0700

    vvvvvvv

--- WebBanPhuKienDienThoai/obj/Debug/net8.0/WebBanPhuKienDienThoai.AssemblyInfo.cs
+++ WebBanPhuKienDienThoai/obj/Debug/net8.0/WebBanPhuKienDienThoai.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanPhuKienDienThoai")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acc9e63c9a901a538a578d79eef24ab93a6ed1c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13be782b9442da7ffa3c017c6467cf038e4c0fdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanPhuKienDienThoai")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanPhuKienDienThoai")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
