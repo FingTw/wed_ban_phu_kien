67978eba5d52a364ad832b6800e5296cc246ca43
Fingtw <nguyenphwngtinh@gmail.com>
2025-04-04 11:22:39 +0700

    them thanht oán paypal cua bao

--- WebBanPhuKienDienThoai/Program.cs
+++ WebBanPhuKienDienThoai/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<PayPalService>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
