68aa8d12d934eac3bdc54e450acf26814e67c79d
ddoan0648 <ddoan0648@gmail.com>
2025-04-06 14:34:34 +0700

    [add] trang trí hoàn chỉnh

--- WebBanPhuKienDienThoai/obj/Debug/net8.0/WebBanPhuKienDienThoai.AssemblyInfo.cs
+++ WebBanPhuKienDienThoai/obj/Debug/net8.0/WebBanPhuKienDienThoai.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanPhuKienDienThoai")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33e0dc968d915e59d9b75d076b801449be2dbad7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbd9f741984eeb0c85db28a0912215814a8aee96")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanPhuKienDienThoai")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanPhuKienDienThoai")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
